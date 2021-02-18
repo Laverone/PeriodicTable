4411489bd2935def1ad760547e6fb37b4757b143
Laverone <kilian.averone@ipilyon.net>
2021-02-18 13:41:18 +0100

    Table element changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,13 @@ export class HomeComponent implements OnInit {
   pageTitle: string = 'Periodic Table';
   errorMessage: string;
 
-    table: ITable[]= []
+    periodics = [
+      { name: 'Hydrogen', mass: 1.008, number: 1, symbol:'H'},
+      { name: 'Helium', mass: 4.0026022, number: 2, symbol:'He'},
+      { name: 'Lithium', mass: 6.94, number: 3, symbol:'Li'},
+      { name: 'Beryllium', mass: 9.01218315, number: 4, symbol:'Be'},
+      { name: 'Boron', mass: 10.81, number: 5, symbol:'B'},
+    ]
 
     constructor(private tableService: TableService){
 
@@ -21,7 +27,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.tableService.getTable().subscribe({
       next: table => {
-        this.table= table
+        this.periodics= table
       },
       error: err => this.errorMessage = err
     })
